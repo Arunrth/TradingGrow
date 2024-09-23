13e48f95f7bdd327538d0a2976a405cfa7725e0f
Arun Rathore <arunrathore@Aruns-MacBook-Air.local>
2024-09-23 22:23:11 +0530

    added svg for android and ios

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+import AllStack from './src/assets/images/allstocks.svg'
+
 const App = () => {
   return (
     <View>
@@ -9,6 +11,7 @@ const App = () => {
         color:'black',
         fontSize:22
       }}>Trading app</Text>
+      <AllStack width={120} height={130} />
     </View>
   )
 }
